7ca73667d9447c356b5866dfd03f24f5ab007419
John Jeffries <jeffries839@gmail.com>
2024-12-06 15:57:42 +0000

    added main game loop calculation to produce a number between 0 and 25

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
-function rungame() {
+// wait for the dom to finish loading before running the game
+// Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
+
+function rungame() {
+// Creates two random numbers between 1 and 25
+let num1 = Math.floor(Math.random() * 25) + 1;
+let num2 = Math.floor(Math.random() * 25) + 1;
 }
 function checkAnswer() {
 
@@ -20,5 +41,5 @@ function displaysubtractQuestion() {
 
 }
 function displaymultiplyQuestion() {
-    
+
 }

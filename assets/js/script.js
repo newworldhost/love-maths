67b6839786dc58ea26efadd6b5fa710d77ce0615
John Jeffries <jeffries839@gmail.com>
2024-12-06 15:37:07 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function rungame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayadditionQuestion() {
+
+}
+function displaysubtractQuestion() {
+
+}
+function displaymultiplyQuestion() {
+    
+}
